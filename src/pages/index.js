f238a1c20335ce87f257ed9b3505584aa259f188
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-05-23 00:01:25 -0300

    chore: Configure initial structure

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Mateus Alcantara</title>
+        <meta
+          name="description"
+          content="Blog pessoal criado para compartilhar conhecimentos sobre programação, jiu jitsu, leitura e estoicismo"
+        />
+      </Head>
+
+      <main>
+        <p>Main</p>
+      </main>
+
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </>
+  );
+}
